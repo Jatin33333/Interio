2b35ef4a8cace4d34b2362dbf94388dce77725a8
Jatin33333 <140475018+Jatin33333@users.noreply.github.com>
2023-08-22 14:55:55 +0530

    added Interio.html

---
+++ interio.js
@@ -0,0 +1,20 @@
+fn1 = () =>{
+    document.getElementById('sider').style.display="block";
+    document.getElementById('close').style.display="block";
+    document.getElementById('bars').style.display="none";
+    document.getElementById('all').style.display="block";
+}
+
+fn2 = () => {
+    document.getElementById('sider').style.display="none";
+    document.getElementById('close').style.display="none";
+    document.getElementById('bars').style.display="block";
+    document.getElementById('all').style.display="block";
+
+}
+
+fn3 = () =>{
+    document.getElementById('sider').style.display="none";
+    document.getElementById('close').style.display="none";
+    document.getElementById('bars').style.display="block";
+}
